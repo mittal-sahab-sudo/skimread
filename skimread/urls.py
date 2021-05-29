7b9971b061d3e7674cb30bb54c71a2722b65f49c
Abhishek <abhishekmittal323@gmail.com>
2021-05-29 10:41:12 +0530

    sitemap added

--- skimread/urls.py
+++ skimread/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 from django.contrib.sitemaps import GenericSitemap # new
 from django.contrib.sitemaps.views import sitemap # new
 from blog.models import BlogPost
+from django.views.generic.base import TemplateView
 
 info_dict = {
     'queryset': BlogPost.objects.all(),
@@ -43,5 +44,11 @@ urlpatterns = [
     path('sitemap.xml', sitemap, 
         {'sitemaps': {'blog': GenericSitemap(info_dict, priority=0.6)}},
         name='django.contrib.sitemaps.views.sitemap'),
+
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
+
     path("<str:title>",views.view_blog, name='view_blog'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
